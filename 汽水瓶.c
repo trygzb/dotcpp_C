aa36a8bccc1c61821a288bb78157b2408585a99e
trygzb <1291074120@qq.com>
2020-03-28 11:36:06 +0800

    Create 汽水瓶.c

---
+++ 汽水瓶.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+/*
+有这样一道智力题：“某商店规定：三个空汽水瓶可以换一瓶汽水。
+小张手上有十个空汽水瓶，她最多可以换多少瓶汽水喝？”答案是5瓶，
+方法如下：
+先用9个空瓶子换3瓶汽水，喝掉3瓶满的，
+喝完以后4个空瓶子，用3个再换一瓶，喝掉这瓶满的，
+这时候剩2个空瓶子。然后你让老板先借给你一瓶汽水，
+喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板。
+如果小张手上有n个空汽水瓶，最多可以换多少瓶汽水喝？
+
+*/
+
+int main(){
+    int n;
+    
+    while(scanf("%d", &n) && n != 0){
+		
+    	int result = test(n,0);
+    	printf("%d\n",result);
+    
+	}
+    return 0;
+}
+
+int test(int n, int count){
+	if(n == 0 || n == 1){
+		return count;
+	}
+	if(n == 2){
+		return count + 1; //向老板借一个瓶子 
+	}else{
+		
+		count += n/3;
+		int temp = n/3;
+		n = n%3;
+		n += temp;
+		test(n, count);
+		
+	}
+	
+}
+
